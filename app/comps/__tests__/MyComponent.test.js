54eb24703bc992e9c0b6aafdd267c4a78db61f41
Tricia Hughes <116763138+triciahughes@users.noreply.github.com>
2023-08-10 14:23:00 -0700

    added comments to test file

--- app/comps/__tests__/MyComponent.test.js
+++ app/comps/__tests__/MyComponent.test.js
@@ -1,14 +1,22 @@
-import React from "react";
-import { render, screen } from "../test-utils";
-import Button from "@mui/material/Button";
+// Importing the necessary modules and components
+import React from "react"; // Base React library
+import { render, screen } from "../test-utils"; // Utilities for rendering components in tests and querying the DOM
+import Button from "@mui/material/Button"; // Button component from Material-UI
 
-describe("Add", () => {
+// Describe block groups related tests under the name "add to list button"
+describe("add to list button", () => {
+  // This is an individual test case
   it("should render a button", () => {
+    // Defining the text we want to search for in the rendered output
     const textToFind = "add to list";
 
+    // Rendering the Button component with the text "add to list"
     render(<Button>add to list</Button>);
+
+    // Using the screen utility to get the button element with the specified text
     const button = screen.getByText(textToFind);
 
+    // The assertion checks if the found button is present in the document
     expect(button).toBeInTheDocument();
   });
 });
